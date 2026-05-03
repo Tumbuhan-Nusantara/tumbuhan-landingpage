b58601c65d1e38a3f6700d878a401dec3ca3f745
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-05-03 19:57:56 +0800

    fix: request code

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,28 +1,7 @@
-// import {getRequestConfig} from 'next-intl/server';
- 
-// export default getRequestConfig(async () => {
-//   // Static for now, we'll change this later
-//   const locale = 'en';
- 
-//   return {
-//     locale,
-//     messages: (await import(`../../messages/${locale}.json`)).default
-//   };
-// });
-
-// import { getRequestConfig } from 'next-intl/server';
-
-// export default getRequestConfig(async ({ locale }) => {
-//   return {
-//     locale: locale ?? 'id',
-//     messages: (await import(`../../messages/${locale ?? 'id'}.json`)).default
-//   };
-// });
-
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async ({ locale }) => {
-  const finalLocale = locale ?? 'id'; // 🔥 fallback WAJIB
+  const finalLocale = locale ?? 'id'; 
 
   return {
     locale: finalLocale,
