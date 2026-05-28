6cf3390f6994bcb22dfd319dae71ce55ec8404c3
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-05-28 16:24:04 +0800

    tidy up code kontak page

--- src/features/Beranda/Kontak/index.tsx
+++ src/features/Beranda/Kontak/index.tsx
@@ -1,40 +1,27 @@
-"use client"
-import { Separator } from "@/components/ui/separator";
-import { Contacts } from "@/src/constants";
+"use client";
+
 import { MessageCircleHeart } from "lucide-react";
 import { useLocale, useTranslations } from "next-intl";
-import Image from "next/image";
 import Link from "next/link";
 
 const KontakFeat = () => {
   const k = useTranslations("contact");
-    useLocale();
+  useLocale();
   return (
     <div className="container mx-auto my-12">
       <div className="grid gap-12 justify-center">
         <div className="grid gap-2 text-center">
           <h1 className="font-bold sm:text-md md:text-2xl text-[#2B593A]">
-            {k('title')}
+            {k("title")}
           </h1>
           <p className="text-muted-foreground sm:text-sm md:text-lg">
-            {k('desc')}
+            {k("desc")}
           </p>
         </div>
         <div className="flex items-center gap-2 justify-center hover:text-gray-600">
-          <Link href="#" >{k('button')}</Link>
-          <MessageCircleHeart size={20}  />
+          <Link href="#">{k("button")}</Link>
+          <MessageCircleHeart size={20} />
         </div>
-      </div>
-      <Separator className="my-12"/>
-      <div className="flex justify-center items-center gap-2 md:gap-8 lg:gap-8">
-        {Contacts.map((logo) => (
-          <div
-            key={logo.id}
-            className="border-[#2B593A] border-2 rounded-full p-4 bg-white duration-500 transition hover:bg-[#c7f7da] cursor-pointer"
-          >
-            <Image width={20} height={20} src={logo.icon} alt={logo.alt} />
-          </div>
-        ))}
       </div>
     </div>
   );
