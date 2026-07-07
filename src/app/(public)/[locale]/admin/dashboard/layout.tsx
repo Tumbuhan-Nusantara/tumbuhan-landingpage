1b1e613122c7b18d1191ea47e9ca5ff839a25f76
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-07-07 10:25:41 +0800

    implement route protection

--- src/app/(public)/[locale]/admin/dashboard/layout.tsx
+++ src/app/(public)/[locale]/admin/dashboard/layout.tsx
@@ -1,4 +1,32 @@
+'use client'
+import { useRouter } from "@/src/i18n/navigation";
 import DashboardWrapper from "./dashwrapper";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "@/lib/axios";
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await axiosInstance.get("/api/v1/auth/me");
+
+        setLoading(false);
+      } catch (error) {
+        router.replace("/admin/login");
+      }
+    };
+
+    checkAuth();
+  }, [router]);
+
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
   return <DashboardWrapper>{children}</DashboardWrapper>;
 }
