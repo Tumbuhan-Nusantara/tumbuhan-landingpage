dd88b54be88ecea5096f0d3a058ef67635c4c4d9
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2026-07-08 20:24:45 +0800

    show all users in dash page

--- src/features/Admin/Dashboard/index.tsx
+++ src/features/Admin/Dashboard/index.tsx
@@ -1,26 +1,44 @@
-'use client'
+"use client";
 import DashboardSkeleton from "@/components/Skeletons/DashboardSk";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { axiosInstance } from "@/lib/axios";
 import { DashMainMenu } from "@/src/constants";
+import { UserDashType } from "@/src/types";
 import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 
 const DashboardFeat = () => {
   const dash = useTranslations("dashmain");
-  const [loading, setLoading] = useState<boolean>(true)
+  const [loading, setLoading] = useState<boolean>(true);
+  const [users, setUsers] = useState<UserDashType[]>([]);
+  const [showAllUsers, setShowAllUsers] = useState<boolean>(false);
+  const displayedUsers = showAllUsers ? users : users.slice(0, 3);
+
+  const getUsers = async () => {
+    try {
+      const response = await axiosInstance.get(`/api/v1/auth/`);
+      const people = response.data.data;
+
+      setUsers(people);
+    } catch (error) {
+      throw error;
+    }
+  };
 
   useEffect(() => {
-  const timer = setTimeout(() => {
-    setLoading(false);
-  }, 1000);
+    const timer = setTimeout(() => {
+      setLoading(false);
+      getUsers();
+    }, 1000);
 
-  return () => clearTimeout(timer);
-}, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-if (loading) {
-  return <DashboardSkeleton />;
-}
+  if (loading) {
+    return <DashboardSkeleton />;
+  }
   return (
     <div className="p-8">
       <h1 className="text-3xl font-bold text-[#1A4D2E] mb-6 px-2">
@@ -43,45 +61,52 @@ if (loading) {
         ))}
       </div>
       <Card className="bg-[url('/image.png')] bg-cover">
-        <h1 className="mx-6 text-[#1A4D2E] font-semibold">Daftar Pengguna Dashboard YTAN</h1>
-        <Card className="mx-6">
-          <div className="flex items-center mx-4 gap-4">
-            <Avatar className="h-12 w-12 rounded-lg">
-              <AvatarImage src="https://github.com/shadcn.png" alt="tes user" />
-              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-[#1A4D2E]">Tjoet Muty</p>
-              <p className="text-muted-foreground">Anggota</p>
+        <h1 className="mx-6 text-[#1A4D2E] font-semibold">
+          Daftar Pengguna Dashboard YTAN
+        </h1>
+        {displayedUsers.map((user) => (
+          <Card key={user.id} className="mx-6">
+            <div className="flex items-center mx-4 gap-4">
+              <Avatar className="h-12 w-12 rounded-lg">
+                <AvatarImage
+                  src="https://github.com/shadcn.png"
+                  alt="tes user"
+                />
+                <AvatarFallback className="rounded-lg">
+                  {user.first_name?.[0]}
+                  {user.last_name?.[0]}
+                </AvatarFallback>
+              </Avatar>
+              <div>
+                <p className="text-[#1A4D2E]">
+                  {user.first_name} {user.last_name}
+                </p>
+                <p className="text-muted-foreground">{user.role}</p>
+              </div>
             </div>
+          </Card>
+        ))}
+        {users.length > 3 && !showAllUsers ? (
+          <div className="flex justify-center my-4">
+            <Button
+              variant="outline"
+              onClick={() => setShowAllUsers(true)}
+              className="border-[#1A4D2E] text-[#1A4D2E]" size="sm"
+            >
+              Tampilkan Lebih Banyak
+            </Button>
           </div>
-        </Card>
-
-        <Card className="mx-6">
-          <div className="flex items-center mx-4 gap-4">
-            <Avatar className="h-12 w-12 rounded-lg">
-              <AvatarImage src="https://github.com/shadcn.png" alt="tes user" />
-              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-[#1A4D2E]">Tjoet Muty</p>
-              <p className="text-muted-foreground">Anggota</p>
-            </div>
+        ) : (
+          <div className="flex justify-center my-4">
+            <Button
+              variant="outline"
+              onClick={() => setShowAllUsers(false)}
+              className="border-[#1A4D2E] text-[#1A4D2E]" size="sm"
+            >
+              Tampilkan Lebih Sedikit
+            </Button>
           </div>
-        </Card>
-
-        <Card className="mx-6">
-          <div className="flex items-center mx-4 gap-4">
-            <Avatar className="h-12 w-12 rounded-lg">
-              <AvatarImage src="https://github.com/shadcn.png" alt="tes user" />
-              <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-            </Avatar>
-            <div>
-              <p className="text-[#1A4D2E]">Tjoet Muty</p>
-              <p className="text-muted-foreground">Anggota</p>
-            </div>
-          </div>
-        </Card>
+        )}
       </Card>
     </div>
   );
